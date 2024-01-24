a568cd4f98fb8841c23db7b34224e1b0cf5bd248
VictoryChibueze <alhajivicci12@gmail.com>
2024-01-24 16:24:08 +0000

    week02 Task

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,17 +7,19 @@
 const fullName = 'Victory Chibueze';
 const currentYear =new Date().getFullYear();
 const profilePicture = '/images/profilee.png';
+
+/* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
 const imageElement = document.querySelector('img');
-/* Step 3 - Element Variables */
+
+
+/* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src',profilePicture);
 imageElement.setAttribute('alt',`Profile image of ${fullName}`)
-
-/* Step 4 - Adding Content */
 
 
 
