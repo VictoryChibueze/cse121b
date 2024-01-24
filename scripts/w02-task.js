6192c7c5fd624152ba383510e0dd76a28e16b071
VictoryChibueze <alhajivicci12@gmail.com>
2024-01-24 17:51:36 +0000

    image

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,13 +6,13 @@
 
 const fullName = 'Victory Chibueze';
 const currentYear = new Date().getFullYear();
-const profilePicture = '/images/placeholder.png';
+const profilePicture = 'images/placeholder.png';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('picture');
+const imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
